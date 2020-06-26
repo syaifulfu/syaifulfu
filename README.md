449a8570b8a3eef31ed2ae75eeba24b44a03d74b
mac <mac@macs-MacBook-Pro.local>
2020-06-26 19:45:12 +0700

    ganti

--- README.md
+++
@@ -1,2 +0,0 @@
-# syaifulfu
-portofolio
--- js/custom.js
+++ js/custom.js
@@ -1,81 +1,165 @@
+(function($) { 
+		  
+$(document).ready(function(){
+	$('.slide').prepend('<div class="patternOverlay"></div>');	
+	
+	$('.skillbar').each(function(){
+		$(this).find('.skillbar-bar').animate({
+			width:$(this).attr('data-percent')
+		},6000);
+	});
+	
+	$('.skillbar').each(function(){
+		$(this).find('.skill-bar-percent').animate({
+			left:$(this).attr('data-percent')
+		},6000);
+	});
+	
+	$('.skillbar').each(function(){
+		$(this).find('.skill-bar-percent').text($(this).attr('data-percent'));
+	});
+	
+	if($(window).width()>767){
+		$('.navbar-wrapper').animate({
+			opacity:0
+		},200);
+		$('.navbar-wrapper').addClass('default-menu');
+		
+	}	
+	
+	$('#nav-show').click(function(x){
+		x.preventDefault();
+		$('.wrapper').removeClass('noGap');
+		$('.navbar-wrapper').animate({
+			opacity:1
+		},200);
+		$('.navbar-wrapper').removeClass('default-menu');
+		$(this).fadeOut();
+	});
+	
+	$('.closeMenu').click(function(x){
+		x.preventDefault();
+		$('.navbar-wrapper').animate({
+			opacity:0
+		},50,function(){$('.navbar-wrapper').addClass('default-menu');});
+		
+		$('#nav-show').fadeIn();
+	});
+	
+	
+});	
 
 
-  /*-------------------------------------------------------------------------------
-    PRE LOADER
-  -------------------------------------------------------------------------------*/
+	
+/*===========================================================*/
+	/*	Preloader 
+/*===========================================================*/	
+//<![CDATA[
+	$(window).load(function() { // makes sure the whole site is loaded
+		$("#status").fadeOut(); // will first fade out the loading animation
+		$("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
+	})
+//]]>
+		  
+// for mobile nav js	
+$(window).load(function(){
+	$('button.navbar-toggle').click(function(){
+		$(this).toggleClass('active');
+		$('.navbar-collapse').slideToggle();										 
+	});					
+});
+// for banner height js
+var windowWidth = $(window).width();
+var windowHeight =$(window).height();
+$('.banner').css({'height':windowHeight });
 
-  $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
-  });
 
-
-
-  /* HTML document is loaded. DOM is ready. 
-  -------------------------------------------*/
-
-  $(document).ready(function() {
-
-
-  /*-------------------------------------------------------------------------------
-    Navigation - Hide mobile menu after clicking on a link
-  -------------------------------------------------------------------------------*/
-
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
+// for portfoli filter jquary
+$(window).load(function(){
+    var $container = $('.portfolioContainer');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
     });
+ 
+    $('.portfolioFilter a').click(function(){
+        $('.portfolioFilter .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    }); 
 
 
-    $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+
+// for portfoli lightbox jquary
+jQuery(function() {
+	var $chosenSheet,
+	$stylesheets = $( "a[id^=theme-]" );
+	
+	// run rlightbox
+	$( ".lb" ).rlightbox();
+	$( ".lb_title-overwritten" ).rlightbox({overwriteTitle: true});
+});
+});
+
+// Somth page scroll
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top -16
+        }, 1000);
+        return false;
+      }
     }
   });
+});
 
 
-
-  /*-------------------------------------------------------------------------------
-    jQuery Parallax
-  -------------------------------------------------------------------------------*/
-
-    function initParallax() {
-    $('#home').parallax("100%", 0.1);
-    $('#about').parallax("100%", 0.3);
-    $('#service').parallax("100%", 0.2);
-    $('#experience').parallax("100%", 0.3);
-    $('#education').parallax("100%", 0.1);
-    $('#quotes').parallax("100%", 0.3);
-    $('#contact').parallax("100%", 0.1);
-    $('footer').parallax("100%", 0.2);
-
-  }
-  initParallax();
-
-
-
-  /*-------------------------------------------------------------------------------
-    smoothScroll js
-  -------------------------------------------------------------------------------*/
-  
-    $(function() {
-        $('.custom-navbar a, #home a').bind('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top - 49
-            }, 1000);
-            event.preventDefault();
-        });
-    });
-  
-
-
-  /*-------------------------------------------------------------------------------
-    wow js - Animation js
-  -------------------------------------------------------------------------------*/
-
-  new WOW({ mobile: false }).init();
-
-
+// Testimonial slider
+$(window).load(function(){
+  $('.flexslider').flexslider({
+	animation: "fade",
+	animationLoop: true,
+	slideshow: true,
+	pauseOnAction: false,
+	slideshowSpeed: 7000,
+	controlNav: true,
+	start: function(slider){
+	  $('body').removeClass('loading');
+	}
   });
+});
 
+
+// we worked slider
+$(window).load(function(){
+      $('.worklogo').flexslider({
+        animation: "slide",
+		slideshow: false,
+        itemWidth: 210,
+        itemMargin: 5,
+        
+      });
+    });
+
+// Skill set 
+
+
+}(jQuery));
